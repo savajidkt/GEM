7a1ae8ad36baa6fc2e2f2abd73e3c88f1af951ea
savajidkt <savaji.dkt@gmail.com>
2023-07-06 12:04:12 +0530

    config test

--- wp-config.php
+++ wp-config.php
@@ -43,33 +43,18 @@ ob_start();
 /** The name of the database for WordPress */
 
 define( 'DB_NAME', "gem" );
-
-
-
 /** Database username */
 
 define( 'DB_USER', "root" );
-
-
-
 /** Database password */
 
 define( 'DB_PASSWORD', "" );
-
-
-
 /** Database hostname */
 
 define( 'DB_HOST', "localhost" );
-
-
-
 /** Database charset to use in creating database tables. */
 
 define( 'DB_CHARSET', 'utf8mb4' );
-
-
-
 /** The database collate type. Don't change this if in doubt. */
 
 define( 'DB_COLLATE', '' );
